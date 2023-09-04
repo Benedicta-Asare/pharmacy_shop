74c09ee4d60b8a9100cf94dfcfd359ade27e8d40
Benedicta-Asare <lilbengilla@gmail.com>
2023-09-04 10:57:40 +0000

    update enums.py

--- pharmacy/enums.py
+++ pharmacy/enums.py
@@ -2,7 +2,6 @@ from enum import Enum
 
 class OrderStatus(str, Enum):
     PENDING = "pending"
-    In_PROGRESS = "in_progress"
+    In_PROGRESS = "in-progress"
     COMPLETED = "completed"
     CANCELLED = "cancelled"
-    
