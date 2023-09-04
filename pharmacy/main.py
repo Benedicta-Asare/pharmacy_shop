28c1e0f316e6271318cc637369cee1dc44de9236
Benedicta-Asare <lilbengilla@gmail.com>
2023-09-04 11:00:46 +0000

    Update main.py

--- pharmacy/main.py
+++ pharmacy/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 from pharmacy.database.core import Base, engine
 from pharmacy.routers import users, inventories, admins
@@ -8,21 +8,21 @@ from pharmacy.routers import users, inventories, admins
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-        # Base.metadata.drop_all(bind=engine)
-        Base.metadata.create_all(bind=engine)
+    # Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(engine)
 
-        yield
+    yield
        
 app = FastAPI(lifespan=lifespan)
-app.include_router(admins.router)
 app.include_router(inventories.router)
 app.include_router(users.router)
+app.include_router(admins.router)
 
 
 @app.get("/ping")
 def ping_pong() -> dict[str, str]:
     return {"message": "pong"}
 
-@app.get("/name")
+@app.get("/name/{first_name}}")
 def get_first_name(first_name: str) -> dict[str, str]:
     return {"name": first_name}
