8e858c663855011719d7178a657883f89b5b111b
Benedicta-Asare <lilbengilla@gmail.com>
2023-09-04 10:10:08 +0000

    Update core.py

--- pharmacy/database/core.py
+++ pharmacy/database/core.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-engine = create_engine("sqlite:///pharmacy.sqlite", echo=True, connect_args={"check_same_thread": False})
+engine = create_engine(
+    "sqlite:///pharmacy.sqlite", 
+    echo=True, 
+    connect_args={"check_same_thread": False}
+)
 
-SessionMaker = sessionmaker(bind=engine, autoflush=False, expire_on_commit=False, autocommit=False,)
+SessionMaker = sessionmaker(
+    bind=engine, 
+    autoflush=False, 
+    expire_on_commit=False, 
+    auto_on_commit=False,)
 
 class Base(DeclarativeBase):
     pass
-
-with SessionMaker.begin() as Session:
-    ...
