e701bdce9380cc27685aa3544cd13be46360ac97
Benedicta-Asare <lilbengilla@gmail.com>
2023-09-06 12:11:13 +0000

    Update main.py

--- pharmacy/main.py
+++ pharmacy/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from pharmacy.database.core import Base, engine
 from pharmacy.routers import users, inventories, admins
@@ -17,6 +18,21 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(inventories.router)
 app.include_router(users.router)
 app.include_router(admins.router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://localhost:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/ping")
